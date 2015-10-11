313449503855dc4e2005221c58cfc3d7ca9fd431
pereirfe <fernandhenriqp@gmail.com>
2015-10-11 19:50:19 -0300

    mod test

--- store.c
+++ store.c
@@ -5,17 +5,17 @@ int main(void){
 
   int length = 160;
   char* fileName = "testFile.dat";
-  float m[10000][2];
+  float m[100][2];
 
   // Write data to file
 
   FILE* file = fopen (fileName, "wb");
 
   for(int i = 0; i< length; i++){
-    m[i][0] = m[i][1] = i;
+    m[i][0] = m[i][1] = 1;
   }
   
-  fwrite(m, sizeof(float), 10000*2, file);
+  fwrite(m, sizeof(float), 100*2, file);
   fclose(file);
 
 }
