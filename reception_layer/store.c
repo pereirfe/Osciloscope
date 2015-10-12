b85de11bb2f1efc602cfd5fc74036a0fe0fb5608
fp-raspi <fernandhenriqp@gmail.com>
2015-10-12 02:52:30 +0000

    99% camada de recepcao

--- reception_layer/store.c
+++ reception_layer/store.c
@@ -1,39 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdint.h>
+#include <bcm2835.h>
 
-#define DATA_L 10000
-
-uint8_t get_byte();
+#define DATA_L 100000
 
 int main(void){
 
   char* fileName = "mydata.dat";
   float m[DATA_L][2];
+  uint8_t rec_buf[2*DATA_L], snd_buf[2*DATA_L]; 
   int i;
   float data;
   // Write data to file
 
+
+  if (!bcm2835_init()) return 1;
+  bcm2835_spi_begin();
+  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+  bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
+  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_8);// The default
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
+  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+
   for(i = 0; i<DATA_L; i++){
-    m[i][0] = i;
+    m[i][0] = i; // TODO: preencher com o tempo da freq de update (testar.. 15M?)
+    snd_buf[2*i] = i%0x100;
+    snd_buf[2*i+1] = 0;
   }
   // preenchendo tabela com valores recuperados do ADC (futuro)
 
   while(1){
-    //TODO CS-LOW	
+
+    puts("connectando ADC...");
+    // Recebe dados do ADC
+    bcm2835_spi_transfernb((char*)snd_buf, (char*)rec_buf, 2*DATA_L); // Funcao ja cuida do CS
+
+    puts("Recebidos! Iniciando processamento");
+    // Processa dados
     for(i = 0; i<DATA_L; i++){
-      m[i][1] = (((float)(get_byte()<<2 + get_byte()))/102.4) - 5.0; // Agregacao 10bits + interpolacao (y=10k/1024 -5)  
+      m[i][1] = (((float)(rec_buf[2*i]<<2 +(rec_buf[2*i+1])>>6))/102.3) - 5.0; // Agregacao 10bits + interpolacao (y=10k/1023 -5)  
+//      printf("(rcv:snd): (%d:%d) - (%d:%d)\n", (rec_buf[2*i]), snd_buf[2*i], (rec_buf[2*i+1]), snd_buf[2*i+1]);
     }
-    //TODO CS-HIGH
+
+    puts("Salvando arquivo...");
 
     FILE* file = fopen (fileName, "wb");
     fwrite(m, sizeof(float), DATA_L*2, file);
     fclose(file);
+    puts("Completed!.");
+    break; // TODO: Tirar
   }
-}
-
-
-uint8_t get_byte(){
-  return 1<<7;
+  return 0;
 }
